a2e4f43dcac01925d8638335bd9b96030224fec6
Shravani Salunke <153487937+Shravanis30@users.noreply.github.com>
2026-07-25 12:46:50 +0530

    Add solution for Maximum Product of Two Digits

---
+++ solutions/easy/Maximum Product of Two Digits.cpp
@@ -0,0 +1,26 @@
+// Title: Maximum Product of Two Digits
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/maximum-product-of-two-digits/
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        int max1 = 0, max2 = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit >= max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
+            }
+
+            n /= 10;
+        }
+
+        return max1 * max2;
+    }
+};
