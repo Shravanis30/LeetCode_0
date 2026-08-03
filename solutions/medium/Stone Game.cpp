cceb16f00b764b663ddcec5b46b7eac54b2815d6
Shravani Salunke <153487937+Shravanis30@users.noreply.github.com>
2026-08-03 22:06:53 +0530

    Add solution for Stone Game

---
+++ solutions/medium/Stone Game.cpp
@@ -0,0 +1,11 @@
+// Title: Stone Game
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/stone-game/
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
